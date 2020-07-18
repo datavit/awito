0ab8a05c31a2bbeb900f4169118564628d124d71
vit <vit.mitelman@gmail.com>
2020-07-18 16:57:43 +0300

    modal windows closing

--- scripts/script.js
+++ scripts/script.js
@@ -3,20 +3,33 @@
 const modalAdd = document.querySelector('.modal__add'),
 addAd = document.querySelector('.add__ad'),
 modalBtnSubmit = document.querySelector ('.modal__btn-submit'),
-modalSubmit = document.querySelector('.modal__submit'); 
+modalSubmit = document.querySelector('.modal__submit'), 
+catalog = document.querySelector('.catalog'),
+modalItem = document.querySelector('.modal__item');
+
+const closeModal = function(event) {
+    const target = event.target;
+    console.log(this);
+    
+    if (target.closest('.modal__close') || 
+    target === this) {
+        this.classList.add('hide');
+        modalSubmit.reset();
+    }
+ };
 
 addAd.addEventListener('click', () => {
     modalAdd.classList.remove('hide');
     modalBtnSubmit.disabled=true;
 })
 
-modalAdd.addEventListener('click', (event) => {
-    const target = event.target;
-    
-    if (target.closest('.modal__close') || 
-    target === modalAdd) {
-        modalAdd.classList.add('hide');
-        modalSubmit.requestFullscreen();
-        modalSubmit.reset();
-    }
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal)
+
+ catalog.addEventListener('click', event => {
+     const target = event.target;
+
+     if (target.closest('.card')) {
+        modalItem.classList.remove('hide');
+     };
  });
